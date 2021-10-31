0196faac9f9f8927a73ebc52712eee03fef6bc54
RemyMach <remygt@hotmail.fr>
2021-10-31 09:15:58 +0100

    add Repository Class

---
+++ src/main/java/fr/remy/cc1/infrastructure/InMemoryUsers.java
@@ -0,0 +1,41 @@
+package fr.remy.cc1.infrastructure;
+
+import fr.remy.cc1.domain.User;
+import fr.remy.cc1.domain.UserId;
+import fr.remy.cc1.domain.Users;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class InMemoryUsers implements Users {
+
+    private final AtomicInteger counter = new AtomicInteger(0);
+    private final Map<UserId, User> data = new ConcurrentHashMap<>();
+
+    @Override
+    public void save(User user) {
+        data.put(user.getUserId(), user);
+    }
+
+    @Override
+    public User byId(UserId userId) {
+        final User user = data.get(userId);
+        if (user == null) {
+            throw new RuntimeException("No user for " + userId.getValue());
+        }
+        return user;
+    }
+
+    @Override
+    public UserId nextIdentity() {
+        return UserId.of(counter.incrementAndGet());
+    }
+
+    @Override
+    public List<User> findAll() {
+        return data.values().stream().collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/fr/remy/cc1/infrastructure/MysqlUsers.java
@@ -0,0 +1,29 @@
+package fr.remy.cc1.infrastructure;
+
+import fr.remy.cc1.domain.User;
+import fr.remy.cc1.domain.UserId;
+import fr.remy.cc1.domain.Users;
+
+import java.util.List;
+
+public class MysqlUsers implements Users {
+    @Override
+    public void save(User user) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    @Override
+    public User byId(UserId userId) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    @Override
+    public UserId nextIdentity() {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    @Override
+    public List<User> findAll() {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+}
