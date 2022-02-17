07422349260fced500accb7f5a65b5db475bd695
Quentin R <rouvilleq@gmail.com>
2022-02-17 22:03:55 +0100

    Fix typing error in InfrastructureExceptionsDictionary

--- src/main/java/fr/remy/cc1/infrastructure/exceptions/InfrastructureExceptionsDictionary.java
+++ src/main/java/fr/remy/cc1/infrastructure/exceptions/InfrastructureExceptionsDictionary.java
@@ -5,6 +5,6 @@ public class InfrastructureExceptionsDictionary {
     public static final NoSuchEntityException TOKEN_NOT_FOUND = new NoSuchEntityException("token_not_found", "the pair user token doesn't match");
     public static final NoSuchEntityException PROJECT_NOT_FOUND = new NoSuchEntityException("project_not_found", "the specified projectId does not match any known project");
     public static final NoSuchEntityException PROJECT_REQUIREMENTS_NOT_FOUND = new NoSuchEntityException("project_requirements_not_found", "the specified projectId does not match any known project requirements");
-    public static final NoSuchEntityException CERTIFICATE_NOT_FOUND = new NoSuchEntityException("certificate_not_found", "the specified certificateId does not match any known certificateId");
+    public static final NoSuchEntityException CERTIFICATE_NOT_FOUND = new NoSuchEntityException("certificate_not_found", "the specified certificateId does not match any known certificate");
 
 }
